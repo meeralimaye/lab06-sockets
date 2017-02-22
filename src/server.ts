b9be7bdb55a0cd66adba39b64ef077cc2dfba8d3
Meera Limaye <mlimaye@uw.edu>
2017-02-22 12:38:59 -0800

    lab complete

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,75 @@
-import net = require('net');//import socket module
+import net = require('net');
 import ip = require('ip');
 
-// define address interface
+
 interface Address { port: number; family: string; address: string; };
 
-// create socket server
+
 let server:net.Server = net.createServer();
 
-// when the server is connected
-server.on('connection', function(socket:net.Socket){
 
-    // when data is sent to the socket
+let clients: net.Socket[] = [];
+
+server.on('connection', function(socket:net.Socket){
+  
+    function broadcast(name:String, message:String){
+        clients.forEach(function(client:net.Socket) {
+            
+            if (client !== socket) {
+                client.write('[' + name + '] ' + message + '\n');
+            }
+        });
+    }
+
+    console.log('connected :' + socket.remoteAddress);
+    clients.push(socket);
+
+    socket.write("Hi what's your name? \n");
+    let name: String = '';
+
     socket.on('data', function(data){
-        //
+        let message:String = data.toString();
+        if(message.length === 0){
+            socket.write('(type something and hit return)\n');
+            return;
+        }
+
+        
+        if (!name) {
+           
+            name = data.toString().substr(0, 10);
+
+           
+            socket.write('Hello ' + name + '!\n');
+            socket.write('Welcome to the chat room, ' + name + '!\n');
+            socket.write('There are ' + clients.length + ' people here.\n');
+            socket.write("Type messages, or type 'exit' at any time to leave.\n");
+        }
+        else {
+           
+            if ('exit' === message) {
+                socket.end();
+            }
+            else {
+              
+                broadcast(name, message);
+            }
+        }
     });
 
-    socket.on('close', function(){
-        // handle client disconnecting
-    })
-
+    
 
 });
 
-//when the server starts listening...
+
 server.on('listening', function() {
-    //output to the console the port number on which we are listening
+   
     var addr:Address = server.address();
+    console.log(addr.address);
     console.log('server listening on port %d', addr.port);
 });
 
-//start the server
+
 server.listen({
   host: ip.address(),
   port: 3000
